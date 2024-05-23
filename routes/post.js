71290ee07369daa27649e29d0ab0f7717912902f
sagnikghosh1999 <saggywebd@gmail.com>
2024-05-24 00:05:44 +0530

    comments

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { createPost, getAllPosts } = require("../controllers/postController");
+const {
+  createPost,
+  getAllPosts,
+  comment,
+} = require("../controllers/postController");
 const { authUser } = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.post("/createpost", authUser, createPost);
 router.get("/getallposts", authUser, getAllPosts);
+router.put("/comment", authUser, comment);
 
 module.exports = router;
