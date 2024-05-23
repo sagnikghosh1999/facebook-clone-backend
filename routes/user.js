23d0718fb7d45d4340db8c1b5c1f6cbe5642a96d
sagnikghosh1999 <saggywebd@gmail.com>
2024-05-23 13:43:07 +0530

    friendship

--- routes/user.js
+++ routes/user.js
@@ -12,6 +12,13 @@ const {
   updateProfilePicture,
   updateCover,
   updateDetails,
+  addFriend,
+  cancelRequest,
+  follow,
+  unfollow,
+  acceptRequest,
+  unfriend,
+  deleteRequest,
 } = require("../controllers/userController");
 const { authUser } = require("../middlewares/auth");
 
@@ -25,9 +32,16 @@ router.post("/finduser", findUser);
 router.post("/sendresetpasswordcode", sendResetPasswordCode);
 router.post("/validateresetcode", validateResetCode);
 router.post("/changepassword", changePassword);
-router.get("/getprofile/:username", getProfile);
+router.get("/getprofile/:username", authUser, getProfile);
 router.put("/updateprofilepicture", authUser, updateProfilePicture);
 router.put("/updatecover", authUser, updateCover);
 router.put("/updatedetails", authUser, updateDetails);
+router.put("/addfriend/:id", authUser, addFriend);
+router.put("/cancelrequest/:id", authUser, cancelRequest);
+router.put("/follow/:id", authUser, follow);
+router.put("/unfollow/:id", authUser, unfollow);
+router.put("/acceptrequest/:id", authUser, acceptRequest);
+router.put("/unfriend/:id", authUser, unfriend);
+router.put("/deleterequest/:id", authUser, deleteRequest);
 
 module.exports = router;
