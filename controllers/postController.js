f15cd6ee7ce1f7b8e78d11de806f4eb725fa6025
sagnikghosh1999 <saggywebd@gmail.com>
2024-05-24 13:59:00 +0530

    posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -13,11 +13,26 @@ exports.createPost = async (req, res) => {
 
 exports.getAllPosts = async (req, res) => {
   try {
-    const posts = await Post.find()
-      .populate("user", "first_name last_name gender picture username")
+    const followingTemp = await User.findById(req.user.id).select("following");
+    const following = followingTemp.following;
+    const promises = following.map((user) => {
+      return Post.find({ user: user })
+        .populate("user", "first_name last_name picture username cover")
+        .populate("comments.commentBy", "first_name last_name picture username")
+        .sort({ createdAt: -1 })
+        .limit(10);
+    });
+    const followingPosts = await (await Promise.all(promises)).flat();
+    const userPosts = await Post.find({ user: req.user.id })
+      .populate("user", "first_name last_name picture username cover")
       .populate("comments.commentBy", "first_name last_name picture username")
-      .sort({ createdAt: "desc" });
-    res.json(posts);
+      .sort({ createdAt: -1 })
+      .limit(10);
+    followingPosts.push(...[...userPosts]);
+    followingPosts.sort((a, b) => {
+      return b.createdAt - a.createdAt;
+    });
+    res.json(followingPosts);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
@@ -43,6 +58,45 @@ exports.comment = async (req, res) => {
       }
     ).populate("comments.commentBy", "picture first_name last_name username");
     res.json(newComments.comments);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+exports.savePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const user = await User.findById(req.user.id);
+    const check = user?.savedPosts.find(
+      (post) => post.post.toString() == postId
+    );
+    if (check) {
+      await User.findByIdAndUpdate(req.user.id, {
+        $pull: {
+          savedPosts: {
+            _id: check._id,
+          },
+        },
+      });
+    } else {
+      await User.findByIdAndUpdate(req.user.id, {
+        $push: {
+          savedPosts: {
+            post: postId,
+            savedAt: new Date(),
+          },
+        },
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deletePost = async (req, res) => {
+  try {
+    await Post.findByIdAndRemove(req.params.id);
+    res.json({ status: "ok" });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
--- routes/post.js
+++ routes/post.js
@@ -3,6 +3,8 @@ const {
   createPost,
   getAllPosts,
   comment,
+  savePost,
+  deletePost,
 } = require("../controllers/postController");
 const { authUser } = require("../middlewares/auth");
 
@@ -11,5 +13,7 @@ const router = express.Router();
 router.post("/createpost", authUser, createPost);
 router.get("/getallposts", authUser, getAllPosts);
 router.put("/comment", authUser, comment);
+router.put("/savepost/:id", authUser, savePost);
+router.delete("/deletepost/:id", authUser, deletePost);
 
 module.exports = router;
